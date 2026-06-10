1629850b26f4907902fee1569407c1d77725e7e3
Mushab Rahman <83585774+Mushabrahman@users.noreply.github.com>
2026-06-10 20:25:47 +0530

    Update BASE_URL for production environment

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -3,4 +3,4 @@ const dev = window.location.hostname === "localhost";
 
 export const BASE_URL = dev
   ? "http://localhost:8000"
-  : "https://www.webtinder.in";
+  : "https://webtinder-backend.onrender.com";
